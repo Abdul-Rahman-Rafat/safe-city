9646ff97b41025085c4c174555bce3bf46460f35
Abdulrahman Rafat Ahmed <140813224+Abdul-Rahman-Rafat@users.noreply.github.com>
2024-04-26 22:27:28 +0300

    ..

--- Re-design/SafeCity/YOLO_Video.py
+++ Re-design/SafeCity/YOLO_Video.py
@@ -6,7 +6,8 @@ from SafeCity.models import Snapshots
 from SafeCity import db
 from flask_login import current_user
 from SafeCity import app
-
+from pathlib import Path
+#output_path=r"\C:\Users\abdul\OneDrive\Documents\GitHub\safe-city\Re-design\SafeCity\static\used_images\alerts_images"
 
 img_name=0 # we should make it random combination
 
@@ -61,6 +62,7 @@ def video_detection(path_x):
             if time_diff >= 10: #detect every ... seconds 
                 
                 file_path = f'{img_name}.png'
+                print(file_path)
                 cv2.imwrite(file_path, img)
                 last_saved_time = current_time
                 img_name = img_name + 1
